9e5f96df8732f1b79e6f00e1fe56f2b4b1a660e4
Ford <86711272+4-Ford204@users.noreply.github.com>
2024-11-19 13:48:18 +0700

    Time: 0 ms (100%), Space: 40.4 MB (44.82%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cs
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cs
@@ -1,8 +1,6 @@
 public class Solution {
     public string GcdOfStrings(string str1, string str2) {
-        if (str1 + str2 != str2 + str1) {
-            return "";
-        }
+        if (str1 + str2 != str2 + str1) return "";
 
         int length1 = str1.Length;
         int length2 = str2.Length;
