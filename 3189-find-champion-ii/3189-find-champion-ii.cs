3bf25abb845b786b6ed2e8f75638d7286e830d1b
Ford <86711272+4-Ford204@users.noreply.github.com>
2024-11-26 11:13:18 +0700

    Time: 2 ms (100%), Space: 91.1 MB (100%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.cs
+++ 3189-find-champion-ii/3189-find-champion-ii.cs
@@ -1,10 +1,20 @@
 public class Solution {
     public int FindChampion(int n, int[][] edges) {
-        HashSet<int> hashSet = new HashSet<int>(Enumerable.Range(0, n).ToArray());
-        
-        for (int i = 0; i < edges.Length; i++)
-            hashSet.Remove(edges[i][1]);
+        int[] degree = new int[n]; 
+        int count = 0;
+        int champion = -1;
 
-        return hashSet.Count == 1 ? hashSet.Single() : -1;
+        for (int i = 0; i < edges.Length; i++) degree[edges[i][1]]++;
+
+        for (int i = 0; i < n; i++) {
+            if (degree[i] == 0) {
+                champion = i;
+                count++;
+            }
+
+            if (count >= 2) break;
+        }
+
+        return count == 1 ? champion : -1;
     }
 }
