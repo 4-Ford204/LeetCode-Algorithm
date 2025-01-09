ce5a1f64cae24aea97474130a702afb8285bb103
Ford <86711272+4-Ford204@users.noreply.github.com>
2025-01-09 08:59:41 +0700

    Time: 1 ms (58.95%), Space: 42.6 MB (36.84%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cs
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cs
@@ -1,5 +1,11 @@
 public class Solution {
     public int PrefixCount(string[] words, string pref) {
-        return words.Where(word => word[..Math.Min(pref.Length, word.Length)].Equals(pref)).Count();
+        return StartsWith(words, pref);
     }
+
+    private int Substring(string[] words, string pref)
+        => words.Where(word => word[..Math.Min(pref.Length, word.Length)].Equals(pref)).Count();
+    
+    private int StartsWith(string[] words, string pref)
+        => words.Where(word => word.StartsWith(pref)).Count();
 }
