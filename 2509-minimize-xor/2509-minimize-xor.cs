3b777a3a89778bc7d9478dba7b534e3b2dc5532d
Ford <86711272+4-Ford204@users.noreply.github.com>
2025-01-15 16:43:30 +0700

    Time: 1 ms (0%), Space: 29.4 MB (0%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.cs
+++ 2509-minimize-xor/2509-minimize-xor.cs
@@ -2,7 +2,7 @@ using System.Runtime.Intrinsics.X86;
 
 public class Solution {
     public int MinimizeXor(int num1, int num2) {
-        return FromOptimalToValid(num1, num2);
+        return BitManipulation(num1, num2);
     }
 
     public int FromOptimalToValid(int num1, int num2) {
@@ -29,5 +29,14 @@ public class Solution {
         }
 
         return result;
+    }
+
+    public int BitManipulation(int num1, int num2) {
+        int k = (int)(Popcnt.PopCount((uint)num2) - Popcnt.PopCount((uint)num1));
+
+        if (k >= 0) for (int i = 0; i < k; i++) num1 |= num1 + 1;
+        else for (int i = 0; i < -k; i++) num1 &= num1 - 1;
+
+        return num1;
     }
 }
