8a58f0463209da7d302b546aa28670cc0bc74edc
Ford <86711272+4-Ford204@users.noreply.github.com>
2025-01-17 09:37:51 +0700

    Time: 2 ms (100%), Space: 61.8 MB (62.5%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool DoesValidArrayExist(int[] derived) {
+        return BruteForce(derived);
+    }
+
+    public bool Aggregate(int[] derived) => derived.Aggregate((x, y) => x ^ y) == 0;
+
+    public bool BruteForce(int[] derived) {
+        int xor = 0;
+
+        foreach (var bit in derived) xor ^= bit;
+        
+        return xor == 0;
+    }
+}
