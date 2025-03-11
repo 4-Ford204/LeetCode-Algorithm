5e93905e2a0e2a9784a52eecbf3926317f7c96a0
Ford <86711272+4-Ford204@users.noreply.github.com>
2025-03-11 16:04:15 +0700

    Time: 30 ms (20.27%), Space: 43.5 MB (13.51%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cs
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int NumberOfSubstrings(string s) {
+        int[] position = new int[3] { -1, -1, -1 };
+        int result = 0;
+
+        for (int i = 0; i < s.Length; i++) {
+            position[s[i] - 'a'] = i;
+            result += 1 + position.Min();
+        }
+
+        return result;
+    }
+
+    private int SlidingWindow(string s) {
         int n = s.Length, left = 0, right = 0, result = 0;
         int[] frequency = new int[3];
 
