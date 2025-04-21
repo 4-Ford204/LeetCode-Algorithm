1d3c32a9d9dae09f72e9cd500a91f234fb619b11
Ford <86711272+4-Ford204@users.noreply.github.com>
2025-04-21 16:55:33 +0700

    Time: 2 ms (90.91%), Space: 68 MB (81.82%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cs
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cs
@@ -1,15 +1,13 @@
 public class Solution {
     public int NumberOfArrays(int[] differences, int lower, int upper) {
-        long prefix = 0, min = 0, max = 0;
+        long prefixDiff = 0, minDiff = 0, maxDiff = 0;
 
         foreach (var difference in differences) {
-            prefix += difference;
-            min = Math.Min(min, prefix);
-            max = Math.Max(max, prefix);
+            prefixDiff += difference;
+            minDiff = Math.Min(minDiff, prefixDiff);
+            maxDiff = Math.Max(maxDiff, prefixDiff);
         }
 
-        long left = lower - min;
-        long right = upper - max;
-        return (int)Math.Max(0, right - left + 1);
+        return (int)Math.Max(0, (upper - lower) - (maxDiff - minDiff) + 1);
     }
 }
