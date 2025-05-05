528d025b04900d69aea1f7b813f6dce0605b6c4d
Ford <86711272+4-Ford204@users.noreply.github.com>
2025-05-05 08:53:59 +0700

    Time: 0 ms (100%), Space: 29.1 MB (59.6%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cs
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cs
@@ -1,14 +1,15 @@
 public class Solution {
     public int NumTilings(int n) {
-        long modulo = 1000000007;
-        long[] dp = new long[n + 1];
-        long[] dpt = new long[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        long modulo = 1_000_000_007;
+        long[] dpd = new long[n + 1], dpt = new long[n + 1];
+        dpd[0] = 1;
+        dpd[1] = 1;
+
         for (int i = 2; i <= n; i++) {
-            dpt[i] = (dpt[i - 1] + dp[i - 2]) % modulo;
-            dp[i] = (2 * dpt[i - 1] + dp[i - 1] + dp[i - 2]) % modulo;
+            dpd[i] = (2 * dpt[i - 1] + dpd[i - 1] + dpd[i - 2]) % modulo;
+            dpt[i] = (dpt[i - 1] + dpd[i - 2]) % modulo;
         }
-        return (int)dp[n];
+
+        return (int)dpd[n];
     }
 }
