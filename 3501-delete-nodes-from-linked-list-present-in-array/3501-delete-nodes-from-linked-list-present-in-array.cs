b0f08a8768bddd7cfda20dad49cbc116da4671fb
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2025-11-01 09:52:07 +0700

    Time: 40 ms (84.85%), Space: 90.5 MB (60.61%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ModifiedList(int[] nums, ListNode head) {
+        var dummy = new ListNode(0, head);
+        var node = dummy;
+        var hashset = new HashSet<int>(nums);
+
+        while (node.next != null) {
+            if (hashset.Contains(node.next.val))
+                node.next = node.next.next;
+            else
+                node = node.next;
+        }
+
+        return dummy.next;
+    }
+}
