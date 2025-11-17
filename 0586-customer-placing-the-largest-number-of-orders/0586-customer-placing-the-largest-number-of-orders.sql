6b3d9ddaf93b32f0aaac7ff1347388b8485e94ea
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2025-11-17 09:30:47 +0700

    Time: 434 ms (88.94%), Space: 0B (100%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+
+SELECT customer_number
+FROM
+(
+    SELECT TOP 1 customer_number, COUNT(*) AS order_total
+    FROM Orders
+    GROUP BY customer_number
+    ORDER BY order_total DESC
+)
+AS T
