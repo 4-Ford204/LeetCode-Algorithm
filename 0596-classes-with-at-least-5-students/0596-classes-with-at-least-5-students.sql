91c0727c87646be3b9ca05d06422e0d9f31f7fec
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2025-11-17 09:54:45 +0700

    Time: 624 ms (44.53%), Space: 0B (100%) - LeetHub

---
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+
+SELECT class
+FROM
+(
+    SELECT class, COUNT(*) AS students
+    FROM Courses
+    GROUP BY class
+    HAVING COUNT(*) >= 5
+)
+AS T
