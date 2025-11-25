5119d0de8cc0307c3add5b21ebba96df7ea858de
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2025-11-25 09:37:58 +0700

    Time: 1 ms (81.82%), Space: 29.3 MB (63.64%) - LeetHub

---
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int SmallestRepunitDivByK(int k) {
+        int remainder = 0;
+
+        for (int i = 1; i <= k; i++) {
+            remainder = (remainder * 10 + 1) % k;
+            if (remainder == 0) return i;
+        }
+
+        return -1;
+    }
+}
