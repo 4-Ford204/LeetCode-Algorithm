bb96901d2805324174520ab83f27fa56dad80559
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2025-12-20 10:49:09 +0700

    Time: 2 ms (100%), Space: 47.2 MB (92.86%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MinDeletionSize(string[] strs) {
+        int n = strs.Length, m = strs[0].Length;
+        int result = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (strs[j][i] < strs[j - 1][i]) {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
