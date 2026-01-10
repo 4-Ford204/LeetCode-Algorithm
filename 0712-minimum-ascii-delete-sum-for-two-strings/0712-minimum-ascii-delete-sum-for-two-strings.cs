f69f92509ba51316e4e2cc7232420258488b1745
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2026-01-10 09:11:37 +0700

    Time: 9 ms (50%), Space: 42.6 MB (87.5%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cs
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cs
@@ -1,28 +1,27 @@
 public class Solution {
     public int MinimumDeleteSum(string s1, string s2) {
-        int[,] dp = new int[s1.Length + 1, s2.Length + 1];
+        int m = s1.Length, n = s2.Length;
+        int[,] dp = new int[m + 1, n + 1];
 
-        for (int i = 1; i <= s1.Length; i++) {
+        for (int i = 1; i <= m; i++) {
             dp[i, 0] = dp[i - 1, 0] + s1[i - 1];
         }
 
-        for (int j = 1; j <= s2.Length; j++) {
+        for (int j = 1; j <= n; j++) {
             dp[0, j] = dp[0, j - 1] + s2[j - 1];
         }
 
-        for (int i = 1; i <= s1.Length; i++) {
-            for (int j = 1; j <= s2.Length; j++) {
-                if (s1[i - 1] == s2[j - 1]) {
-                    dp[i, j] = dp[i - 1, j - 1];
-                } else {
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i, j] = s1[i - 1] == s2[j - 1] ?
+                    dp[i - 1, j - 1] :
                     dp[i, j] = Math.Min(
-                        dp[i - 1, j] + s1[i - 1], 
+                        dp[i - 1, j] + s1[i - 1],
                         dp[i, j - 1] + s2[j - 1]
                     );
-                }
             }
         }
 
-        return dp[s1.Length, s2.Length];
+        return dp[m, n];
     }
 }
