b5decbf00ff67a1eafa9f68081ef4a257b7839c3
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2026-04-13 09:49:32 +0700

    Time: 0 ms (100%), Space: 42.1 MB (86.21%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int GetMinDistance(int[] nums, int target, int start) {
+        int result = int.MaxValue;
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == target) result = Math.Min(result, Math.Abs(i - start));
+        }
+
+        return result;
+    }
+}
