984dbf1b70efb801f2873273a6bbb5cec8416190
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2026-05-23 13:47:25 +0700

    Time: 0 ms (100%), Space: 42.8 MB (95%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool Check(int[] nums) {
+        int n = nums.Length, increasing = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) increasing++;
+        }
+
+        if (nums[n - 1] > nums[0]) increasing++;
+
+        return increasing <= 1;
+    }
+}
