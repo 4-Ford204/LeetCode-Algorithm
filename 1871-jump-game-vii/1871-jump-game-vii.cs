edd5cc1cf073e55a30d73ea80696fce921df9cb0
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2026-05-25 09:29:33 +0700

    Time: 4 ms (96.55%), Space: 50.2 MB (60.34%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cs
+++ 1871-jump-game-vii/1871-jump-game-vii.cs
@@ -1,24 +1,17 @@
 public class Solution {
     public bool CanReach(string s, int minJump, int maxJump) {
-        int n = s.Length;
-        int[] prefix = new int[n], arr = new int[n];
-        arr[0] = 1;
+        int n = s.Length, count = 0;
+        var dp = new bool[n];
+        dp[0] = true;
 
         if (s[n - 1] != '0') return false;
 
-        for (int i = 0; i < minJump; i++) prefix[i] = 1;
-
-        for (int i = minJump; i < n; i++) {
-            int start = i - maxJump, end = i - minJump;
-
-            if (s[i] == '0') {
-                int sum = prefix[end] - (start <= 0 ? 0 : prefix[start - 1]);
-                arr[i] = sum != 0 ? 1 : 0;
-            }
-
-            prefix[i] = prefix[i - 1] + arr[i];
+        for (int i = 1; i < n; i++) {
+            if (i >= minJump && dp[i - minJump]) count++;
+            if (i > maxJump && dp[i - maxJump - 1]) count--;
+            if (s[i] == '0' && count > 0) dp[i] = true;
         }
 
-        return arr[n - 1] == 1;
+        return dp[n - 1];
     }
 }
