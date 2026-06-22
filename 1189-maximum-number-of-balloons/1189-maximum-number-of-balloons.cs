0b221d4065ce39c74268da08bc042df125a101cb
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2026-06-22 08:34:21 +0700

    Time: 1 ms (82%), Space: 39.8 MB (84%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MaxNumberOfBalloons(string text) {
+        int result = int.MaxValue;
+        var arr = new int[5];
+
+        foreach (var t in text) {
+            switch (t) {
+                case 'b': arr[0]++; break;
+                case 'a': arr[1]++; break;
+                case 'l': arr[2]++; break;
+                case 'o': arr[3]++; break;
+                case 'n': arr[4]++; break;
+            }
+        }
+
+        arr[2] /= 2;
+        arr[3] /= 2;
+
+        foreach (var item in arr) result = Math.Min(result, item);
+
+        return result;
+    }
+}
