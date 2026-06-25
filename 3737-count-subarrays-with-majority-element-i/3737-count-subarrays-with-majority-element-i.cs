1872adf3c1e77e3b4a3a891b57c51870c00c80d2
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2026-06-25 09:35:20 +0700

    Time: 37 ms (100%), Space: 50 MB (60%) - LeetHub

---
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int CountMajoritySubarrays(int[] nums, int target) {
+        int n = nums.Length, result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;          
+            for (int j = i; j < n; j++) {
+                count += (nums[j] == target ? 1 : -1);
+                if (count > 0) result++;
+            }
+        }
+
+        return result;
+    }
+}
