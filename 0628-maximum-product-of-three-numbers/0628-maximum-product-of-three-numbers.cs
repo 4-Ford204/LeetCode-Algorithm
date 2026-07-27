eeb806be9a4ce45395bc03e695a17983e2da56d6
Lê Tự Huỳnh Duy <86711272+4-Ford204@users.noreply.github.com>
2026-07-27 09:40:24 +0700

    Time: 1 ms (100%), Space: 52.6 MB (27.43%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cs
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cs
@@ -1,11 +1,17 @@
 public class Solution {
     public int MaximumProduct(int[] nums) {
-        Array.Sort(nums);
-        int n = nums.Length;
+        int L1 = int.MaxValue, L2 = int.MaxValue;
+        int R1 = int.MinValue, R2 = int.MinValue, R3 = int.MinValue;
 
-        return Math.Max(
-            nums[n - 1] * nums[n - 2] * nums[n - 3],
-            nums[n - 1] * nums[0] * nums[1]
-        );
+        foreach (var num in nums) {
+            if (num < L1) (L1, L2) = (num, L1);
+            else if (num < L2) L2 = num;
+
+            if (num > R3) (R1, R2, R3) = (R2, R3, num);
+            else if (num > R2) (R1, R2) = (R2, num);
+            else if (num > R1) R1 = num;
+        }
+
+        return Math.Max(L1 * L2 * R3, R1 * R2 * R3);
     }
 }
